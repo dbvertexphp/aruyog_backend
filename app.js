7b7ff22cf5aebeb89c4daea8696eebaa88788ec0
suraj7968 <php4.dbvertex@gmail.com>
2024-06-18 13:18:54 +0530

    suraj code live

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: "./.env" });
 const __dirname1 = path.resolve();
-import { connectDB } from "./config/db.js";
+import connectDB from "./config/db.js";
 
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 
--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
-const mongoose = require("mongoose");
-
+import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
@@ -9,11 +8,11 @@ const connectDB = async () => {
       //useFindAndModify: false
     });
 
-    //console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1); // Exit with a non-zero status code to indicate an error
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
