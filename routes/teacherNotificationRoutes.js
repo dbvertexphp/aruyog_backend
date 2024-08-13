33954a18c5e4c81651d8e2f54c277b3d65c1481a
dbvertexphp <php4.dbvertex@gmail.com>
2024-08-13 20:39:41 +0530

    suraj code live

--- routes/teacherNotificationRoutes.js
+++ routes/teacherNotificationRoutes.js
@@ -8,7 +8,7 @@ const teacherNotificationsRoutes = express.Router();
 // GET notifications for a user
 teacherNotificationsRoutes.route("/getTeacherNotifications").get(protect, getTeacherNotifications);
 teacherNotificationsRoutes.route("/sendCourseNotification").post(protect, sendCourseNotification);
-teacherNotificationsRoutes.route("/getMissingAttendanceDays").post(protect, getMissingAttendanceDays);
+teacherNotificationsRoutes.route("/getMissingAttendanceDays").post(getMissingAttendanceDays);
 teacherNotificationsRoutes.route("/resetCourseMeeting").post(protect, resetCourseMeeting);
 teacherNotificationsRoutes.route("/getTeacherNotificationsByAdmin").get(protect, getTeacherNotificationsByAdmin);
 
